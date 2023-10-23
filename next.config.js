381c6673ae80e753892b8caa1f68547a62b73295
Piers <piers.pollard@smartwyre.com>
2023-10-23 12:17:39 +0100

    adds page and modal

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
