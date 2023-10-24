4608fd1b9b53dd65c556986d6b0c7fa4ae41013e
Piers <piers.pollard@smartwyre.com>
2023-10-24 12:17:09 +0100

    Removes modal requirement

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ To complete this test, applicants are expected to spend one hour and complete th
 
 1. Clone the repo and create your own repository (do not fork)
 2. Add pagination such that each page would show 10 items per page rather than all items.
-3. Allow one click navigation when viewing a single item to the next result, rather than forcing the use to close and open the next item.
+3. Unit test where necessary
 4. Fix general issues and refactor with solid principles.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
 "use client"
 
 import React from 'react';
-import Image from 'next/image'
-import styles from './page.module.css'
 import { useEffect, useState } from 'react'
-
-const Modal = (props: any) => {
-  return (
-    <div className={styles.modal}>
-      <div>{props.data.name}</div>
-      <div onClick={props.closeModal}>x</div>
-      <Image src={props.data.image} alt={props.data.name} width="50"  height="50"/>
-    </div>
-  )
-}
 
 export default function Home() {
   const [d, setD] = useState<any[]>([]);
-  const [modal, setModal] = useState(null);
   useEffect(() => {
     fetch(
       'https://api.spacexdata.com/v4/crew/query', 
@@ -40,12 +27,8 @@ export default function Home() {
     <main style={{display: 'flex', 'alignItems': 'center', "justifyContent": 'center'}}>
       <div>
         <h1>Crew Members</h1>
-        {d.map(d => (
-          <div onClick={() => setModal(d.id)}>{d.name} {d.status}</div>
-        ))}
+        {d.map(d => <div>{d.name} {d.status}</div>)}
       </div>
-
-      {modal && <Modal data={d.find(d => d.id === modal)} closeModal={() => setModal(null)} />}
     </main>
   )
 }
